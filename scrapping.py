21681d97dcbd82e2b5a13c40cf07f8d9c468eff5
Xecus <abo.elfotouh@live.com>
2019-05-20 11:02:18 +0200

    Finish weather script and add notes for both scrapping and weather scripts

--- scrapping.py
+++ scrapping.py
@@ -4,73 +4,117 @@ import time
 import pandas as pd
 
 def Countries():
-
+    '''
+    Function for scrapping list of counties available on th web.
+    '''
+    #Connect to the web-site and scrapping all countries data
     try :
         url = requests.get('https://www.weather-forecast.com/countries')
     except:
         print('Error in reading page!')
         quit()
-
+    #get all contents
     page = soup(url.content, 'html.parser')
-
+    # Find countries div
     containers = page.findAll('div',{'class':'mapctrytab-cont'})
-
+    # intiate countries dataframe
     countries = pd.DataFrame(columns=['country','link'])
+    # help counter for getting links
     counter = 0
+    # get all names and links for every country
     for con in containers:
         links = con.findAll('a')
         for link in links:
+            # Add country name and link to dataframe
             countries.loc[counter] = [link.text,'https://www.weather-forecast.com{0}'.format(link['href'])]
+            # counter for the next link
             counter += 1
 
     print('Countries Page has scrapped successfully')
+    # return counteries dataframe
     return countries
 
 def Cities(country):
+    '''
+    Function for scrapping list of cities available on the web
+    for the selected country
+    '''
+    # initiate dataframe for cities data
     cities = pd.DataFrame(columns=['city','link'])
+    #Connect to the web-site and scrapping cities data for selected country
     url = requests.get(country['link'])
+    #get all contents
     page = soup(url.content, 'html.parser')
+    # Find cities section
     section = page.findAll('section',{'class':'b-wrapper'})
+    # get all cities links
     links = section[0].findAll('a')
+    # help counter for getting links
     counter = 0
+    # get all names and links for every city
     for link in links:
+        # Add city name and link to dataframe
         cities.loc[counter] = [link.text,'https://www.weather-forecast.com{0}'.format(link['href'])]
         counter +=1
 
     print('Cities Page has scrapped successfully')
+    # return cities dataframe
     return cities
 
 def City(city):
+    '''
+    Function for scrapping weather details for the selected city
+    '''
+    # initiate dataframe for weather data
     weather = pd.DataFrame(columns=['days','dates','time','HighTemp','LowTemp'])
+    #Connect to the web-site and scrapping weather data for selected city
     url = requests.get(city['link'])
+    #get all contents
     page = soup(url.content, 'html.parser')
-
+    # get spans for days names
     days = page.findAll('span',{'class':'b-forecast__table-days-name'})
+    # initiate available times
     times = ['AM','PM','NIGHT']
+    # get spans for days dates
     dates = page.findAll('span',{'class':'b-forecast__table-days-date'})
+    # get spans for temperatures
     temps = page.findAll('span',{'class':'temp b-forecast__table-value'})
 
+    # get all days names and dates
     for count in range(len(days)):
+        # get all temperatures for all days and times [High Temperatures, Low Temperatures]
         for wcount in  range(len(times)):
+            # get temperatures for count day
             mycount = (count*3)+wcount
             weather.loc[mycount]=[
                     days[count].text,
                     dates[count].text,
                     times[wcount],
                     temps[mycount].text,
-                    temps[mycount+len(days)*3].text
+                    temps[mycount+len(days)*3].text #get the low temperatures
                     ]
 
     print('Weather Page has scrapped successfully')
+    # return weather dataframe
     return weather
 
 def main():
+    '''
+    This main function to test the script alone
+    '''
+    # scrapping countries
     countries = Countries()
+    # select the first country
     country = {'country':countries['country'][0],'link':countries['link'][0]}
-    time.sleep(5)
+    # pause for one second to avoid web blocking
+    time.sleep(1)
+    # scrapping cities for selected country
     cities = Cities(country)
+    # select the first city
     city = {'city':cities['city'][0],'link':cities['link'][0]}
-    time.sleep(5)
+    # pause for one second to avoid web blocking
+    time.sleep(1)
+    # scrapping weather for selected city
     weather = City(city)
 
 
--- weather.py
+++ weather.py
@@ -5,83 +5,139 @@ and place.
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
 import requests
 import pandas as pd
 import time
 import scrapping
 
+# intitial scrapping for countries
 countries = scrapping.Countries()
+# select initial country
 country = {'country':countries['country'][0],'link':countries['link'][0]}
+# pause for 1 second to avoid web blocking
 time.sleep(1)
+# intitial cities scrapping for selected country
 cities = scrapping.Cities(country)
+# select initial city
 city = {'city':cities['city'][0],'link':cities['link'][0]}
+# pause for 1 second to avoid web blocking
 time.sleep(1)
+# weather scrapping for selected city
 weather = scrapping.City(city)
 
+# create dash application
 app = dash.Dash()
 
+# create dash layout
 app.layout = html.Div([
-    html.Div([
-        html.Div([
+    html.Div([ html.H1(children='Weather Scrapping for weather-forecast.com',
+        style={'textAlign': 'center'}),
+        html.Div([ html.H3(children='Select Country: ',
+            style={'width': '30%'}),
             dcc.Dropdown(
                 id='country-id',
                 options=[{'label': i, 'value': i} for i in countries['country']],
                 value=countries['country'][0]
-            )], style={'width': '30%', 'display': 'inline-block'}),
-        html.Div([
+            )], style={'width': '30%'}),
+        html.Div([html.H3(children='Select City: ',
+            style={'width': '30%'}),
             dcc.Dropdown(
                 id='city-id',
                 options=[{'label': i, 'value': i} for i in cities['city']],
                 value=cities['city'][0]
-            )], style={'width': '30%', 'display': 'inline-block'}),
-        html.Div([
+            )], style={'width': '30%'}),
+        html.Div([ html.Br(),
             html.Button(
                 id='submit-button',
                 n_clicks=0,
                 children='Submit',
                 style={'fontSize':28}
-            )], style={'width': '30%', 'display': 'inline-block'}),
+            )], style={'width': '30%'}),
     ], style={'padding':10}),
     html.Div([
-        dcc.Graph(
-            id='weather-plot',
-            figure={
-                'data': [
-                    go.Scatter(
-                        x = weather['days'],
-                        y = weather['HighTemp'],
-                        mode='markers'
-                    )
-                ],
-                'layout': go.Layout(
-                    title = 'Weather for nine days',
-                    xaxis = {'title': 'Days'},
-                    yaxis = {'title': 'Temperatures'},
-                    hovermode='closest'
-                )
-            }
-        )], style={'width':'90%', 'float':'center'}),
+        dcc.Graph(id='weather-plot')
+        ], style={'width':'90%', 'float':'center'}),
 ])
+
 @app.callback(Output('city-id','options'),[Input('country-id','value')])
 def cities_options(SelectedCountry):
+    '''
+    Function to get the list of cities for the selected country
+    '''
+    # use globla cities variable
     global cities
+    # get country data for the selected one
     GetCountry = {'country':SelectedCountry,
             'link':countries['link'][countries.loc[countries.country==SelectedCountry].index[0]]}
+    # scrapping the cities for the selected country
     cities = scrapping.Cities(GetCountry)
+    # return cities as options for Cities Dropdown
     return [{'label': i, 'value': i} for i in cities['city']]
 
 @app.callback(Output('city-id','value'),[Input('city-id','options')])
 def cities_value(SelectedCities):
+    '''
+    Function to set the first city on the list as a value
+    '''
     return cities['city'][0]
 
 @app.callback(
     Output('weather-plot', 'figure'),
     [Input('submit-button', 'n_clicks')],
     [State('city-id', 'value')])
-def update_graph(SelectedCity):
-    
+def update_graph(n_clicks,SelectedCity):
+    '''
+    Function to get the weather details for the selected city
+    and return graph data
+    '''
+    global weather
+    # get city data for the selected one
+    GetCity = {'city':SelectedCity,
+        'link':cities['link'][cities.loc[cities.city==SelectedCity].index[0]]}
+    # scrapping weather data for the selected city
+    weather = scrapping.City(GetCity)
+    # change dates colum type to string instead of number
+    weather['dates']=pd.Series(weather['dates'],dtype=str)
+
+    # create traces for AM times
+    AmHigh = go.Scatter(x=weather[weather['time'] == 'AM']['dates'] + ' - '
+                        + weather[weather['time'] == 'AM']['days'],
+                        y=weather[weather['time'] == 'AM']['HighTemp'],
+                        mode='lines',name='High Temperatures at AM')
+    AmLow = go.Scatter(x=weather[weather['time'] == 'AM']['dates'] + ' - '
+                        + weather[weather['time'] == 'AM']['days'],
+                        y=weather[weather['time'] == 'AM']['LowTemp'],
+                        mode='lines',name='Low Temperatures at AM')
+    # create traces for PM times
+    PmHigh = go.Scatter(x=weather[weather['time'] == 'PM']['dates'] + ' - '
+                        + weather[weather['time'] == 'PM']['days'],
+                        y=weather[weather['time'] == 'PM']['HighTemp'],
+                        mode='lines',name='High Temperatures at PM')
+    PmLow = go.Scatter(x=weather[weather['time'] == 'PM']['dates'] + ' - '
+                        + weather[weather['time'] == 'PM']['days'],
+                        y=weather[weather['time'] == 'PM']['LowTemp'],
+                        mode='lines',name='Low Temperatures at PM')
+    # create traces for NIGHT times
+    NightHigh = go.Scatter(x=weather[weather['time'] == 'NIGHT']['dates'] + ' - '
+                            + weather[weather['time'] == 'NIGHT']['days'],
+                            y=weather[weather['time'] == 'NIGHT']['HighTemp'],
+                            mode='lines',name='High Temperatures at NIGHT')
+    NightLow = go.Scatter(x=weather[weather['time'] == 'NIGHT']['dates'] + ' - '
+                            + weather[weather['time'] == 'NIGHT']['days'],
+                            y=weather[weather['time'] == 'NIGHT']['LowTemp'],
+                            mode='lines',name='Low Temperatures at NIGHT')
+    # list all traces on data list
+    data = [AmHigh, AmLow, PmHigh, PmLow, NightHigh, NightLow]
+    # create graph layout
+    layout = go.Layout(title = '{0} Weather for 9 days'.format(SelectedCity),
+                    xaxis = {'title': 'Days'},
+                    yaxis = {'title': 'Temperatures C'},
+                    hovermode='closest'
+                    )
+    # return graph figure
+    return {'data':data, 'layout':layout}
 
 if __name__ == '__main__':
     app.run_server()
